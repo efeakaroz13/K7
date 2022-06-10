2306cc97123f2586566c62b07fd614b094020ccb
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-06-10 12:10:09 +0300

    sign_up python

--- auth.py
+++ auth.py
@@ -15,8 +15,20 @@ class auth:
             except:
                 return  {"login":False,"password":False,"email":False}
 
+    def sign_up(email,password):
+        out = os.popen("./auth-module register {} {}".format(email,password)).read()
+        try:
+            out.split("200")[1]
+            return {"success":True}
+        except:
+            try:
+                out.split("500")[1]
+                return {"success":False}
+            except:
+                return {"success":None}
+        
 
-print(auth.sign_in("efeakaroz13","1234"))
+
 
 
     
