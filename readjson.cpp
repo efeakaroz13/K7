f3895da38df0dac5b7c4730bbe03a6a942e0711b
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-22 23:43:23 +0300

    getting the array indexes of elements

--- readjson.cpp
+++ readjson.cpp
@@ -1,10 +1,41 @@
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <array>
+
 using json = nlohmann::json;
+using namespace std;
+
 
 int main(void) {
-    std::ifstream f("sort.json");
+
+    ifstream f("sort.json");
     json data = json::parse(f);
-    std::cout<< data;
+    json articlelist = data["listofarticles"];
+    int articlecounter = articlelist.size();
+    cout<<"Articles counted:";
+    cout<<articlecounter<<"\n";
+
+
+
+    // for (auto it = articlelist.begin(); it != articlelist.end(); ++it) {
+        
+    //     cout<<it.value()<<"\n";
+    // }
+    for (auto eid = articlelist.begin(); eid != articlelist.end(); ++eid)
+    {
+        json elem = eid.value();
+        int n = sizeof(articlelist)/sizeof(articlelist[0]);
+        int indexof = 0;
+        while (indexof < n)
+        {
+            if (articlelist[indexof] == elem) {
+                break;
+            }
+            indexof++;
+        }
+        cout<<indexof<<"\n";
+    }
+
+    
 }
