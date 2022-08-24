97d36c45295fb72fbf363413e5fe823e57a08790
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-24 14:19:32 +0300

    reading database url from secret json

--- sortjson.cpp
+++ sortjson.cpp
@@ -2,6 +2,67 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <array>
+#include <string>
+#include <sstream>
+
+using json = nlohmann::json;
+using namespace std;
+
+void bubbleSort(json& arr, int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j]["points"] > arr[j + 1]["points"])
+				swap(arr[j], arr[j + 1]);
+}
+
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+void printJsonArray(json& jsonarr){
+	for (auto eid = jsonarr.begin(); eid != jsonarr.end(); ++eid)
+    {
+    	cout<<eid.value();
+    	cout<<"\n\n";
+    }
+}
+
+
+int main()
+{
+	ifstream f2("secret.json");
+    json credentialdataread = json::parse(f2);
+    string databaseurl = credentialdataread["firebaseConfig"]["databaseURL"];
+    cout<<databaseurl<<"\n";
+
+	ifstream f("sort.json");
+    json data = json::parse(f);
+    json articlelist = data["listofarticles"];
+
+    int articlecounter = articlelist.size();
+    cout<<"Articles counted:";
+    cout<<articlecounter<<"\n";
+    bubbleSort(articlelist,articlecounter);
+    data["listofarticles"]= articlelist;
+
+    ofstream o("sort.json");
+    o << setw(4) << data << endl;
+	return 0;
+}
+
+
+/*
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <array>
 
 using json = nlohmann::json;
 using namespace std;
@@ -38,13 +99,17 @@ int main()
 	ifstream f("sort.json");
     json data = json::parse(f);
     json articlelist = data["listofarticles"];
-    printJsonArray(articlelist);
+
     int articlecounter = articlelist.size();
     cout<<"Articles counted:";
     cout<<articlecounter<<"\n";
     bubbleSort(articlelist,articlecounter);
-    printJsonArray(articlelist);
+    data["listofarticles"]= articlelist;
+
+    ofstream o("sort.json");
+    o << setw(4) << data << endl;
 	return 0;
 }
 
 
+*/
