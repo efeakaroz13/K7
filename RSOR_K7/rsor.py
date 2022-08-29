8c2e9f9c5ae413df641ffa392119c693aa5ceca2
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-29 14:13:58 +0300

    python wrapper

---
+++ RSOR_K7/rsor.py
@@ -0,0 +1,20 @@
+import json
+import random
+import os
+
+stringstuff = ["I","Z","K","R","P","A","O","U","Y","N"]
+
+class RSOR:
+	def __init__(self):
+		self.session = f"{random.choice(stringstuff)}{random.choice(stringstuff)}{random.choice(stringstuff)}{random.randint(1,2345345546)}"
+
+	def search(self,query):
+
+		os.system(f"""./wikipedia  "{query}" {self.session} """)
+		os.system(f"""./duckie  "{query}" {self.session} """)
+		os.system(f"""./googler  "{query}" {self.session} """)
+		jsontoreturn = json.loads(open(self.session+".json","r").read())
+		os.system("rm {self.session}.json")
+		return jsontoreturn
+
+		
--- RSOR_K7/wikipediasearch.cpp
+++ RSOR_K7/wikipediasearch.cpp
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]){
 
 				}
 			}else{
-				cout<<"No results!\n";
+				cout<<"No results!\n ";
 			}
 			
 		}
